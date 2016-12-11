214379525e5d29e6d943d2e0a5bf0b60eec31ac2
Hassan <hfalahi@gmail.com>
2016-12-11 03:58:20 -0800

    remove links dynamically from post content

--- app/home/posts.component.ts
+++ app/home/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, AfterViewChecked, Input } from '@angular/core';
+import { Component, AfterViewInit, AfterViewChecked, Input, ElementRef, ChangeDetectorRef } from '@angular/core';
 import {HomeService} from './home.service'
 import {Observable} from 'rxjs/Rx';
 
@@ -12,19 +12,38 @@ export class PostsComponent {
     posts: any[] = [];
     tags: any[] = [];
     pageNumber = 0;
-    constructor(public homeServie: HomeService) {
+    constructor(public homeServie: HomeService, private elRef: ElementRef, private cdRef: ChangeDetectorRef) {
         homeServie.getTags().then((data) => {
             this.tags = data;
         });
         this.loadPosts();
+    }
+    isViewInitialized = false;
+    ngAfterViewInit() {
+        this.isViewInitialized = true;
+        if (this.posts.length > 0) {
+            this.updateHrefs();
+        }
+    }
+
+    updateHrefs() {
+        this.elRef.nativeElement.querySelectorAll('p > a.more-link').forEach((e: any) => {
+            debugger;
+            e.href = '';
+            //[routerLink]="['/home/post', post.id]"
+            e.routerLink = '/home/post/10';
+            // e.text = '';
+            // e.remove();
+          });
+
     }
 
     loadPosts() {
         this.pageNumber++;
         let self = this;
         this.homeServie.getPosts(this.pageNumber, 3).then((data: any[]) => {
-            if(data.length == 0)
-             return;
+            if (data.length == 0)
+                return;
 
             if (self.posts.length == 0)
                 self.posts = data;
@@ -33,6 +52,10 @@ export class PostsComponent {
                     self.posts.push(element);
                 });
 
+            this.cdRef.detectChanges(); 
+            if (this.isViewInitialized) {
+                this.updateHrefs();
+            }
         });
     }
 }
